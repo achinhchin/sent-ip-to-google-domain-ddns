f197fe101d4462d03dd166306129db5656066d4b
Chinchin <chinchin.anawach@gmail.com>
2022-02-19 16:12:00 +0700

    finish 0.0.a.0

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,6 @@ class Main {
         setInterval(async () => {
             this.cache = Cache.get();
             this.homeIp = await this.getHomeIp();
-
             
             if (this.homeIp != this.cache) {
                 this.updateIpToDdns();
@@ -37,7 +36,10 @@ class Main {
     public updateIpToDdns(): void {
         this.getToken.forEach(tokenData => {
             axios.get(`https://${tokenData.user}:${tokenData.pass}@domains.google.com/nic/update?hostname=${tokenData.domain}&myip=${this.homeIp}`);
-            fs.writeFileSync('log/main.txt', 'ip changed from (' + this.cache + ') to (' + this.homeIp + ')' + ',  time : ' + new Date().toLocaleString() + '\n', {encoding: 'utf8', flag: 'a'});
+            if (!fs.existsSync('./log/')) {
+                fs.mkdirSync('./log/');
+            }
+            fs.writeFileSync('./log/main.txt', 'ip changed from (' + this.cache + ') to (' + this.homeIp + ') at ' + tokenData.domain + ',  time : ' + new Date().toLocaleString() + '\n', {encoding: 'utf8', flag: 'a+'});
         });
     }
 
@@ -57,9 +59,10 @@ class Main {
 
 class Cache {
     public static get(): string {
-        let data: string = fs.readFileSync('./data/cache.json', { encoding: 'utf8', flag: 'w+' });
+        let data: string = fs.readFileSync('./data/cache.txt', { encoding: 'utf8', flag: 'rs+' });
+        
         if (data != '') {
-            return JSON.parse(data);
+            return data;
         } else {
             return '';
         }
